1344f4896cc346b67ed11d875e031c20a3c57efa
Sokolova88 <[sokolova.studies@gmail.com]>
2023-11-02 19:45:19 +0200

    Add a logic to work option's slider

---
+++ js/swiper.js
@@ -0,0 +1,41 @@
+const swiperOptions = new Swiper('.options__swiper', {
+  direction: 'horizontal',
+  loop: true,
+  grabCursor: true,
+  slideToClickedSlide: true,
+  lazy: true,
+  preloadImages: false,
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+  },
+
+  pagination: {
+    el: '.swiper__pagination',
+    type: 'fraction',
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+
+  mousewheel: {
+    sensitivity: 1,
+  },
+
+  speed: 500,
+});
