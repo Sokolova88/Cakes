8752f10136125c389a51a3e91d8c7b6aac9b0f56
Sokolova88 <[sokolova.studies@gmail.com]>
2023-11-04 23:43:21 +0200

    Add a logic for the feedback's slider

--- js/swiper.js
+++ js/swiper.js
@@ -23,7 +23,49 @@ const swiperOptions = new Swiper('.options__swiper', {
   },
 
   pagination: {
-    el: '.swiper__pagination',
+    el: '.options__swiper--pagination',
+    type: 'fraction',
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+
+  mousewheel: {
+    sensitivity: 1,
+  },
+
+  speed: 500,
+});
+
+const swiperFeedback = new Swiper('.feedback__swiper', {
+  direction: 'horizontal',
+  loop: true,
+  grabCursor: true,
+  slideToClickedSlide: true,
+  lazy: true,
+  preloadImages: false,
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+  },
+
+  pagination: {
+    el: '.feedback__swiper--pagination',
     type: 'fraction',
   },
 
